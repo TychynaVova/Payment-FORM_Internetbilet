3a7aa294b3fc8491c9c3b0ee9677e5c9510d9b51
Tychyna Vova <tychinavova@gmail.com>
2023-12-27 12:27:18 +0200

    Delete Forms_Internetbilt directory
    directory change

--- Forms_Internetbilt/js/main.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	Inputmask({"mask": "9999 9999 9999 9999", "placeholder": ""}).mask($('#cardnumber')[0]);
-	Inputmask({"mask": "999", "placeholder": ""}).mask($('#cardcvv')[0]);
-});
